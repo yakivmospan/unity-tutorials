a6056619ab0f244dd7d3818b0db5bcf7e84994d5
Yakiv Mospan <yakiv.mospan@gmail.com>
2017-01-21 22:41:03 +0200

    Space Shooter tutorial
    1. Completed Moving the player

---
+++ Space Shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Tilt{
+	public float horizontal, verticalUp, verticalDown;
+}
+
+[System.Serializable]
+public class Boundary{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Tilt tilt;
+	public Boundary boundary;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVetical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVetical);
+		rb.velocity = movement * speed; 
+
+		rb.position = new Vector3 (
+			Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+			0.0f,
+			Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+		);
+			
+		rb.rotation = Quaternion.Euler (
+			rb.velocity.z * (rb.velocity.z > 0 ? tilt.verticalUp : tilt.verticalDown),
+			0.0f,
+			rb.velocity.x * -tilt.horizontal);
+	}
+
+}
