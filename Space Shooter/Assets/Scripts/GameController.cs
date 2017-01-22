c0cef03370cda395ab3f2ff3ba454deefb7f5d7c
Yakiv Mospan <yakiv.mospan@gmail.com>
2017-01-22 23:53:34 +0200

    Space Shooter tutorial
    1. Completed Game Controller

---
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazardAsteroid;
+	public Vector3 spawnTransform;
+
+	// Use this for initialization
+	void Start () {
+		SpawnWaves ();
+	}
+	
+	void SpawnWaves () {
+		Instantiate (
+			hazardAsteroid,
+			new Vector3(Random.Range(-spawnTransform.x, spawnTransform.x), spawnTransform.y, spawnTransform.z), 
+			Quaternion.identity
+		);
+	}
+}
