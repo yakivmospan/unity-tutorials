70ef4d5ab6b8491de03b4296fb8cbeb4037435df
Yakiv Mospan <yakiv.mospan@gmail.com>
2017-01-22 23:13:31 +0200

    Space Shooter tutorial
    1. Completed Boundary
    2. Completed Creating hazards
    3. Completed Explosions

---
+++ Space Shooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+	public GameObject explision;
+	public GameObject playerExplision;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag != "Boundary") {
+			Destroy(other.gameObject);
+			Destroy(gameObject);
+
+			if (other.tag == "Player") {
+				Instantiate (playerExplision, other.transform.position, other.transform.rotation);
+			}
+			Instantiate (explision, transform.position, transform.rotation);
+		}
+	}
+}
--- Space Shooter/Assets/Scripts/RandomRotator.cs
+++ Space Shooter/Assets/Scripts/RandomRotator.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoverForward : MonoBehaviour {
-	
-	public float speed;
+public class RandomRotator : MonoBehaviour {
+
+	public float tumble;
+
 	private Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
-		rb = GetComponent<Rigidbody> ();
-		rb.velocity = transform.forward * speed;
+		rb = GetComponent<Rigidbody>();
+		rb.angularVelocity = Random.insideUnitSphere * tumble;
 	}
 }
